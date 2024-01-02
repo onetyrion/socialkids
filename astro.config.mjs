b16476627c73d814c2be0370a379f84c2a99c305
Diego Tapia <dtapia@falabella.cl>
2024-01-02 00:31:41 -0300

    Feat: Add comming soon page

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import { settings } from './src/data/settings';
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: settings.site,
+  integrations: [sitemap(), icon({
+    include: {
+      // Include only three `mdi` icons in the bundle
+      mdi: ['instagram', 'linkedin', 'whatsapp'],
+      // Include all `uis` icons
+      uis: ['*']
+    }
+  })],
+});
---
+++ src/data/socials.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/lafonosocialkids/',
+  },
+  {
+    title: 'LinkedIn',
+    icon: 'mdi:linkedin',
+    url: 'https://www.linkedin.com/in/flgafranciscaveloso/',
+  },
+  {
+    title: 'Whatsapp',
+    icon: 'mdi:whatsapp',
+    url: 'https://api.whatsapp.com/send?phone=56956177762&text=Hola%20necesito%20informaci%C3%B3n%20acerca%20de%20las%20terapias',  
+  }
+]
