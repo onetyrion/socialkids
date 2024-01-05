c5abe449b24cf2c0993511e474af1e535c139dd6
Diego Tapia <dtapia@falabella.cl>
2024-01-05 16:38:59 -0300

    Feat: H1 edit css

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -2,5 +2,5 @@ export const settings = {
   site: 'https://socialkids.cl',
   name: 'lafonosocialkids',
   title: 'Social Kids | Fonoudiología a domicilio',
-  description: 'Terapias a domicilio en Santiago para niños con trastornos de lenguaje, habla y voz, incluyendo autismo y síndrome de Down. Mejoramos su calidad de vida y potenciamos su desarrollo.',
+  description: 'Terapias fonoaudiológicas a domicilio en Santiago para niños con trastornos de lenguaje, habla y voz, incluyendo autismo y síndrome de Down.',
 }
