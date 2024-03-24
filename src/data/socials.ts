787ec61303ce019138b663ad4871c188bcf47973
Diego Tapia <dtapia@falabella.cl>
2024-03-23 23:44:29 -0300

    Feat: Add shorcut to mailto and update images

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -17,6 +17,6 @@ export const socials = [
   {
     title: "Mail",
     icon: "mdi:email",
-    url: "mailto:contacto@socialkids.cl?subject=Solicitud%20de%20Informaci%C3%B3n%20sobre%20Terapias%20Fonoudiologicas&body=%3Ch2%3EContacto%20para%20Informaci%C3%B3n%20sobre%20Terapias%20Fonoudiologicas%3C%2Fh2%3E%0D%0A%0D%0A%3Cp%3EEstimado%20equipo%20de%20Social%20Kids%2C%3C%2Fp%3E%0D%0A%0D%0A%3Cp%3EMe%20dirijo%20a%20ustedes%20para%20solicitar%20informaci%C3%B3n%20detallada%20sobre%20las%20terapias%20fonoudiologicas%20dirigidas%20a%20ni%C3%B1os%20con%20trastornos%20del%20habla%20y%20comunicaci%C3%B3n.%20Estoy%20interesado%2Fa%20en%20conocer%20m%C3%A1s%20acerca%20de%20los%20servicios%20que%20ofrecen%20y%20c%C3%B3mo%20pueden%20beneficiar%20a%20mi%20hijo%2Fa.%3C%2Fp%3E%0D%0A%0D%0A%3Cp%3EQuedo%20atento%2Fa%20a%20su%20pronta%20respuesta.%20Agradezco%20de%20antemano%20la%20atenci%C3%B3n%20y%20el%20tiempo%20dedicado%20a%20esta%20solicitud.%3C%2Fp%3E%0D%0A%0D%0A%3Cp%3ESaludos%20cordiales%2C%3C%2Fp%3E",
+    url: "https://bit.ly/3VydIOc",
   },
 ];
