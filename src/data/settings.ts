7fcf238ff8123ddb3b453c3a5f99fdaefc3b479b
Diego Tapia <dtapia@falabella.cl>
2024-03-25 10:13:52 -0300

    Feat: delete h2

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
     site: 'https://socialkids.cl',
     name: 'lafonosocialkids',
-    title: 'SocialKids | Fonoudiología a domicilio',
+    title: 'SocialKids | Fonoudiología a domicilio en Puerto Varas y alrededores',
     description: 'SocialKids | Terapias fonoaudiológicas a domicilio en Puerto Varas para niños con trastornos de lenguaje, habla y voz, incluyendo autismo, síndrome de Down y otras patologías.',
   }
