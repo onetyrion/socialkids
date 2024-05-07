c49e110dffd0d9e0a19f305274d98320eb6d73c5
Diego Tapia <dtapia@falabella.cl>
2024-05-07 13:57:02 -0400

    Feat: Add id

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -1,18 +1,22 @@
 export const navItems = [
   {
     href: "#",
+    id: "home",
     text: "Inicio",
   },
   {
     href: "#experiences",
+    id: "experiences",
     text: "Experiencias",
   },
   {
     href: "#therapy",
+    id: "therapy",
     text: "Nuestras terapias",
   },
   {
     href: "#about-me",
+    id: "about-me",
     text: "Sobre mí",
   },
 ];
--- src/data/socials.ts
+++ src/data/socials.ts
@@ -3,20 +3,24 @@ export const socials = [
     title: "Instagram",
     icon: "mdi:instagram",
     url: "https://www.instagram.com/lafonosocialkids/",
+    id: "instagram_button",
   },
   {
     title: "LinkedIn",
     icon: "mdi:linkedin",
     url: "https://www.linkedin.com/in/flgafranciscaveloso/",
+    id: "linkedin_button",
   },
   {
     title: "Whatsapp",
     icon: "mdi:whatsapp",
     url: "https://api.whatsapp.com/send?phone=56979627693&text=Hola%20necesito%20informaci%C3%B3n%20acerca%20de%20las%20terapias",
+    id: "whatsapp_button",
   },
   {
     title: "Mail",
     icon: "mdi:email",
     url: "https://bit.ly/3VydIOc",
+    id: "mail_button",
   },
 ];
